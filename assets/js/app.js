5cbf2505c4e7830516ec2926c7cb845054d07952
aloegartenn <97531085+aloegartenn@users.noreply.github.com>
2022-02-23 23:42:05 +0200

    done

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+function calculation(){
+    let firstNumber = + inputFirstNumber.value;
+    let secondNumber = + inputSecondNumber.value;
+    let sum = plus.checked;
+    let difference = minus.checked;
+    let multiplication = multiply.checked;
+    let division = divide.checked;
+
+    let answer = 0;
+    if(sum){
+        answer= firstNumber + secondNumber
+    }
+    else if(difference){
+        answer= firstNumber - secondNumber
+    }
+    else if(multiplication){
+        answer= firstNumber * secondNumber
+    }
+    else if(division){
+        answer= firstNumber / secondNumber
+    }
+
+    result.innerHTML = answer;
+    
+}
